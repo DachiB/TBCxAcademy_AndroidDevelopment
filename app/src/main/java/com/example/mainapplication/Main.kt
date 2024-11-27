df9a0a7379bc1d0ef3a075de5592fedb1b886633
DachiB <dachi.burdiashvili@gmail.com>
2024-11-27 23:20:49 +0400

    minor code improvements

--- app/src/main/java/com/example/mainapplication/Main.kt
+++ app/src/main/java/com/example/mainapplication/Main.kt
@@ -7,12 +7,14 @@ fun main() {
 
 fun String.numberizer(): Float {
     var appendString = ""
+
     for (i in this) {
         if (i.isDigit()) {
             val result = i.toString()
             appendString += result
         }
     }
+
     return if (this.isNotEmpty()) {
         appendString.toFloat()
     } else {
@@ -20,28 +22,39 @@ fun String.numberizer(): Float {
     }
 }
 
+
 fun play() {
     println("შეიყვანე X:")
     val inputX: String = readln()
+
     println("შეიყვანე Y:")
     val inputY: String = readln()
+
     val resultX = inputX.numberizer()
     val resultY = inputY.numberizer()
-    val resultZ = resultX / resultY
-    println("X: $resultX, Y: $resultY")
-    println("X და Y განაყოფი არის: $resultZ")
-    tryAgain()
+
+    if (resultY == 0.0f) {
+        println("0ზე გაყოფა არშეიძლება სცადეთ თავიდან")
+        play()
+    } else {
+        val resultZ = resultX / resultY
+        println("X: $resultX, Y: $resultY")
+        println("X და Y განაყოფი არის: $resultZ")
+        tryAgain()
+    }
 }
+
 
 fun tryAgain() {
     println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>?")
     val answer: String = readln()
-    if (answer.lowercase() == "y") {
-        play()
-    } else if (answer.lowercase() == "n") {
-        exitProcess(0)
-    } else {
-        println("შეიყვანე სწორი პასუხი Y/N")
-        tryAgain()
+
+    when (answer.lowercase()) {
+        "y" -> play()
+        "n" -> exitProcess(0)
+        else -> {
+            println("შეიყვანე სწორი პასუხი Y/N")
+            tryAgain()
+        }
     }
 }
