fdaaeae743e583c9976d89444c3fe4035cfd7620
DachiB <dachi.burdiashvili@gmail.com>
2024-12-30 03:26:06 +0400

    Homework 12 init 2

---
+++ app/src/main/java/com/example/mainapplication/Product.kt
@@ -0,0 +1,10 @@
+package com.example.mainapplication
+
+data class Product(
+    val orderId: String,
+    val trackingNumber: String,
+    val quantity: String,
+    val orderStatus: String,
+    val date: String,
+    val price: String
+)
